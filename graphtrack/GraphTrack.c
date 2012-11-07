8b5b7d7c9b4c763e90342e72cc2e7a3e5f74dc54
analogbit <mbruckner807@gmail.com>
2012-11-07 10:16:05 -0600

    working

--- graphtrack/GraphTrack.c
+++ graphtrack/GraphTrack.c
@@ -5,19 +5,21 @@
 int main(int argv, char** argc)
 {
 	Track track;
-	float heights[5]={10.0,1.0,5.0,4.0,10.0};
+	float heights[5]={0.0,3.0,4.0,6.0,12.0};
 	track.heights=heights;
 	track.nheights=5;
 	track.spaces=1.0;
 	State init;
-	init.pos=3.0;
-	init.vel=-1.0;
+	init.pos=4.0;
+	init.vel=0.0;
 	track.init=init;
-	track.g=1.0;
-	State * graph=calc(&track,1.0,10);
+	track.g=10.0;
+	State * graph=calc(&track,0x0.1p0,20);
 	int i,j;
-	for(i=0;i<10;i++){
-		for(j=0;j<graph[i].pos;j++){
+	printf("|---------|---------|---------|---------|\n");
+	for(i=0;i<20;i++){
+		for(j=0;j<graph[i].pos*10;j++){
+			
 			printf("-");
 		}
 		printf("*\n");
--- graphtrack/Track.c
+++ graphtrack/Track.c
@@ -3,13 +3,36 @@
 #include <math.h>
 #include <stdlib.h>
 
+static int quadratic(float A,float B,float C,float Y,float* sol)
+{
+	if((B*B-4*A*(C-Y))<0)return 0;
+	
+	float n=sqrt(B*B-4*A*(C-Y));
+	if(n==0.0){
+		*sol=-B/(2*A);
+		return 1;
+	}
+}
+
 State* calc(Track* track,float interval,int num)
 {
 	State* out=malloc(sizeof(State)*num);
 	int i;
 	State cstate=track->init;
-	float ctime;
+	float ctime=0.0;
 	for(i=0;i<num;i++){
+		float gtime=i*interval;
+		while(ctime!=gtime){
+			int place=(int)(cstate.pos/track->spaces);
+			if(place<0||place>track->nheights-1){ctime=gtime;break;}
+			float acc=track->g*track->spaces/(track->heights[place]-track->heights[place+1]);
+			float a=acc/2.0;
+			float b=cstate.vel;
+			float c=cstate.pos;
+			cstate.pos+=a*interval*interval+b*interval;
+			cstate.vel+=a*interval*2;
+			ctime+=interval;
+		}
 		
 		out[i]=cstate;
 	}
