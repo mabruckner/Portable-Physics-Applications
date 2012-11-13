dd9f798fda79e8f5ed98148cf09d2ff3ffef26ea
analogbit <mbruckner807@gmail.com>
2012-11-13 11:31:04 -0600

    UI

--- cvision/UI.c
+++ cvision/UI.c
@@ -13,8 +13,5 @@ void init_UI()
 	builder=gtk_builder_new();
 	gtk_builder_add_from_file(builder,"builder.ui",NULL);
 	GtkWidget* window=gtk_builder_get_object(builder,"window");
-	GtkWidget* button=gtk_builder_get_object(builder,"button1");
-	GtkWidget* image=gtk_image_new_from_file("wire.bmp");
-	gtk_button_set_image(button,image);
 	g_signal_connect(window,"destroy",G_CALLBACK(gtk_main_quit),NULL);
 }
